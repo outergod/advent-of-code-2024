ff61bae7104353851a2ca88d46adce0a0b92e620
Alexander Dorn <ad@not.one>
2024-12-06 10:02:38 +0100

    Day 6 finished

--- src/day_6.clj
+++ src/day_6.clj
@@ -6,10 +6,38 @@
 (def input (slurp (io/resource "day-6")))
 
 (defn read-input [input]
-  (into {} (apply concat
-                  (map-indexed (fn [y line]
-                                 (keep-indexed (fn [x c]
-                                                 (case c \^ [[x y] :guard] \. [[x y] :floor] \# [[x y] :obstacle] \space nil)) line))
-                               (s/split-lines input)))))
+  (let [rows (s/split-lines input)
+        extent-y (count rows)
+        extent-x (count (first rows))
+        coordinates (for [y (range 0 extent-y) x (range 0 extent-x)] [x y])
+        direction (cycle [[0 -1] [1 0] [0 1] [-1 0]])]
+    (reduce (fn [acc [x y]]
+              (case (get-in rows [y x])
+                \^ (-> acc (assoc :guard [x y]) (update :floor conj [x y]))
+                \# (update acc :obstacles conj [x y])
+                \. (update acc :floor conj [x y])
+                acc))
+            {:obstacles #{} :floor #{} :direction direction} coordinates)))
 
-(read-input example)
+(defn solve-1 [input]
+  (let [{:keys [obstacles floor guard direction]} (read-input input)]
+    (loop [guard guard direction direction visited #{guard}]
+      (let [new (mapv + guard (first direction))]
+        (cond
+          (floor new) (recur new direction (conj visited new))
+          (obstacles new) (recur guard (next direction) visited)
+          :else (count visited))))))
+
+(defn solve-2 [input]
+  (let [{:keys [obstacles floor guard direction]} (read-input input)
+        solve (fn [floor obstacles]
+                (loop [guard guard direction direction visited #{[guard (first direction)]}]
+                  (let [new (mapv + guard (first direction))
+                        visit [new (first direction)]]
+                    (cond
+                      (visited visit) true
+                      (floor new) (recur new direction (conj visited visit))
+                      (obstacles new) (recur guard (next direction) visited)
+                      :else false))))]
+    (count (filter identity (map (fn [pos] (solve (disj floor pos) (conj obstacles pos)))
+                                 (disj floor guard))))))
