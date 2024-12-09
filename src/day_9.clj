05bc620086f917a26b58ec6ad1e44ffc34fadaa9
Alexander Dorn <ad@not.one>
2024-12-09 13:16:45 +0100

    Day 9 finished

--- src/day_9.clj
+++ src/day_9.clj
@@ -34,7 +34,33 @@
   (let [disk (defrag-1 (make-disk-1 input))]
     (reduce + (map * (range) (take-while #(not= % -1) disk)))))
 
-(let [parsed (map #(-> % str parse-long) example)
-      disk (mapv #(repeat %2 %1) (range) (take-nth 2 parsed))
-      space (take-nth 2 (rest parsed))]
-  [disk space])
+(defn make-disk-2 [input]
+  (let [parsed (map #(-> % str parse-long) input)]
+    (dissoc
+     (reduce (fn [{:keys [position] :as disk} [i n]]
+               (assoc
+                (if (even? i)
+                  (update disk :files conj {:size n :position position})
+                  (update disk :space conj {:size n :position position}))
+                :position (+ position n)))
+             {:files [] :space [] :position 0} (map vector (range) parsed))
+     :position)))
+
+(defn defrag-2 [disk]
+  (reduce (fn [{:keys [files space] :as disk} i]
+            (let [{file-size :size file-position :position} (get files i)]
+              (if-let [x (some (fn [[x {:keys [size]}]] (when (>= size file-size) x))
+                               (map vector (range) (take-while (fn [{:keys [position]}] (< position file-position)) space)))]
+                (let [{:keys [position size]} (get space x)]
+                  {:files (assoc-in files [i :position] position)
+                   :space (if (= file-size size)
+                            (vec (concat (subvec space 0 x) (subvec space (inc x))))
+                            (-> space (update-in [x :size] - file-size) (update-in [x :position] + file-size)))})
+                disk)))
+          disk (reverse (range (count (disk :files))))))
+
+(defn solve-2 [input]
+  (let [disk (defrag-2 (make-disk-2 input))]
+   (reduce + (map (fn [i {:keys [position size]}]
+                    (reduce + (map (partial * i) (range position (+ position size)))))
+                  (range) (disk :files)))))
