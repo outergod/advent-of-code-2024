345a246bafbb730a86923db02a6642ddc78ade1c
Alexander Dorn <ad@not.one>
2024-12-11 12:03:14 +0100

    Day 11 finished

--- src/day_11.clj
+++ src/day_11.clj
@@ -1,8 +1,7 @@
 (ns day-11
   (:require [clojure.java.io :as io]
             [clojure.string :as s]
-            [clojure.math :as math]
-            [clojure.walk :as walk]))
+            [clojure.math :as math]))
 
 (def example (s/trim-newline (slurp (io/resource "day-11-example"))))
 (def input (s/trim-newline (slurp (io/resource "day-11"))))
@@ -15,22 +14,19 @@
 
 (defn evolve [stone]
   (cond
-    (zero? stone) 1
+    (zero? stone) [1]
     (even? (int (inc (math/log10 stone)))) (split-stone stone)
-    :else (* stone 2024)))
+    :else [(* stone 2024)]))
 
 (def mem-evolve (memoize evolve))
 
-(defn walk [stones] (if (vector? stones) stones (evolve stones)))
+(def solve (memoize (fn [n stone]
+                      (if (= 1 n)
+                        (count (mem-evolve stone))
+                        (reduce + (map (partial solve (dec n)) (mem-evolve stone)))))))
 
 (defn solve-1 [input]
-  (count (flatten (nth (iterate (partial walk/postwalk walk) (parse input)) 25))))
+  (reduce + (map (partial solve 25) (parse input))))
 
 (defn solve-2 [input]
-  (count (flatten (nth (iterate (partial walk/postwalk walk) (parse input)) 75))))
-
-(defn solve-2 [input]
-  (count (nth (iterate #(->> % (map mem-evolve) flatten) (parse input)) 75)))
-
-(solve-2 input)
-(map count (take 30 (iterate #(->> % (map mem-evolve) flatten) [0])))
+  (reduce + (map (partial solve 75) (parse input))))
