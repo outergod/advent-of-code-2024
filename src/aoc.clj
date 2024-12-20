f9ecd4fdd9ce85a899c0cfece4551908462cc48d
Alexander Dorn <ad@not.one>
2024-12-20 16:58:56 +0100

    Day 20 finished

--- src/aoc.clj
+++ src/aoc.clj
@@ -1,4 +1,7 @@
-(ns aoc)
+(ns aoc
+  (:require
+   [clojure.string :as s]
+   [clojure.data.priority-map :refer [priority-map-keyfn]]))
 
 (defmacro traced-map [s f & colls]
   `(let [n# (dec (apply max (map count (list ~@colls))))]
@@ -7,4 +10,78 @@
             (apply ~f xs#))
           (range) ~@colls)))
 
+(defmacro traced-mapcat [s f & colls]
+  `(let [n# (dec (apply max (map count (list ~@colls))))]
+     (mapcat (fn [i# & xs#]
+               (println "trace" ~s i# "/" n#)
+               (apply ~f xs#))
+             (range) ~@colls)))
+
 (defn fix [f] (fn g [& args] (apply f g args)))
+
+(defn parse-layout [input]
+  (let [layout (s/split-lines (s/trim-newline input))
+        extent-y (count layout)
+        extent-x (count (first layout))]
+    (reduce (fn [acc [x y]]
+             (case (get-in layout [y x])
+               \# (update acc :walls conj [x y])
+               \S (assoc acc :start [x y])
+               \E (assoc acc :end [x y])
+               \. (update acc :floor conj [x y])))
+           {:walls #{} :floor #{} :extents [extent-y extent-y]}
+           (for [y (range 0 extent-y) x (range 0 extent-x)] [x y]))))
+
+(defn euclidian-distance
+  ([[x y]] (Math/sqrt (+ (Math/pow x 2) (Math/pow y 2))))
+  ([[x0 y0] [x1 y1]] (euclidian-distance [(- x1 x0) (- y1 y0)])))
+
+(defn in-limits? [[x y] [w h]] (and (>= x 0) (>= y 0) (< x w) (< y h)))
+
+(defn neighbors [[x y]]
+  (map (fn [[a b]] [(+ x a) (+ y b)]) [[0 1] [1 0] [0 -1] [-1 0]]))
+
+(defn a*-seq [[x0 y0] [x1 y1] [w h] obstacles]
+  (let [in-limits? #(in-limits? % [w h])]
+    (letfn [(h [[x y]] (euclidian-distance [x y] [x1 y1]))
+            (f [[x y] g] (+ g (h [x y])))
+            (n [[x y] g parents fringe]
+              (let [parents (conj parents [x y])]
+                (keep (fn [[x y]]
+                        (let [g (inc g)
+                              cost (f [x y] g)
+                              [prev-cost] (fringe [x y])]
+                          (when-not (and prev-cost (<= prev-cost cost))
+                            [[x y] [cost g parents]])))
+                      (filter in-limits? (neighbors [x y])))))]
+      (iterate
+       (fn [{:keys [mode fringe closed goal] :as state}]
+         (if (#{:finish :end} mode)
+           (assoc state :mode :end)
+           (let [[[x y] [cost distance parents] :as node] (peek fringe)
+                 [[x-goal y-goal] [cost-goal _ parents-goal] :as node-goal] (peek goal)]
+             (cond (= [x y] [x1 y1])
+                   (-> state (update :fringe pop) (update :goal conj node))
+
+                   (and (not-empty node-goal)
+                        (or (empty? node)
+                            (< cost-goal cost)))
+                   (-> state
+                       (assoc :mode :finish)
+                       (assoc :goal (conj parents-goal [x-goal y-goal])))
+
+                   node
+                   (let [candidates (n [x y] distance parents fringe)]
+                     (-> state
+                         (assoc :fringe (into (pop fringe) (remove (comp closed first) candidates)))
+                         (update :closed conj [x y])
+                         (update :touched into (filter obstacles (map first candidates)))))))))
+       {:mode :continue
+        :fringe (priority-map-keyfn first [x0 y0] [(h [x0 y0]) 0 []])
+        :closed obstacles
+        :goal (priority-map-keyfn first)
+        :touched #{}}))))
+
+(defn a* [start goal obstacles extents limit]
+  (last (take limit (take-while (comp #{:continue :finish} :mode)
+                                (a*-seq start goal extents obstacles)))))
